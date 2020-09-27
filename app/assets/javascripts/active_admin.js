b276ef16d526399f9f82b42e34db67dd62f0ceca
haydenlinder <haydenlinder@gmail.com>
2020-09-27 01:40:03 -0700

    Edit js

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,12 +1,12 @@
 //= require active_admin/base
 document.addEventListener('DOMContentLoaded', function() {
-    const container = document.getElementById('titlebar_right').firstElementChild
-    let inner = container.innerHTML + '<span class="action_item"> <a href="/admin/lines/import">Import Lines</a></span>'
+    var container = document.getElementById('titlebar_right').firstElementChild
+    var inner = container.innerHTML + '<span class="action_item"> <a href="/admin/lines/import">Import Lines</a></span>'
     container.innerHTML = inner;
-    const wrapper = document.getElementById('wrapper');
-    const header = document.getElementById('header');
-    const titleBar = document.getElementById('title_bar');
-    const newHeader = document.createElement('div');
+    var wrapper = document.getElementById('wrapper');
+    var header = document.getElementById('header');
+    var titleBar = document.getElementById('title_bar');
+    var newHeader = document.createElement('div');
     newHeader.id = 'new-header';
     newHeader.appendChild(header);
     newHeader.appendChild(titleBar);
